bcc31ccc51af05a028ad3b774933a2b21680c28d
Francisco Wallison <franciscowallison@gmail.com>
2021-04-24 09:31:58 -0300

    configurando amiente deteste

--- nodejs/sonar-project.js
+++ nodejs/sonar-project.js
@@ -7,9 +7,10 @@ sonarqubeScanner(
             'sonar.sources':  'src',
             'sonar.tests':  '__tests__',
             'sonar.inclusions'  :  '**/src/**', // Entry point of your code
-            'sonar.exclusions'  :  '**/node_modules/**', // Entry point of your code
+            'sonar.exclusions'  :  '**/node_modules/**,**public**', // Entry point of your code
             'sonar.test.inclusions':  '__tests__/**/*.spec.js,__tests__/**/*.spec.jsx,__tests__/**/*.test.js,__tests__/**/*.test.ts',
             'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
+            'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml',
             'sonar.login':'admin',
             'sonar.password':'admin'
         }
